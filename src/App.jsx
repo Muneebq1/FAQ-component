028c05e57deff9ee2be090241cf7972456fdc198
muneeb <muneebshahzad244@gmail.com>
2022-12-15 12:30:51 +0500

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,6 @@ import { faAngleRight, faAngleDown, faQ } from '@fortawesome/free-solid-svg-icon
 
 function App() {
 
-  const clickHandler = () => {
-    document.getElementById("right").classList.toggle("hidden")
-    document.getElementById("down").classList.toggle("hidden")
-    document.getElementById("ans").classList.toggle("hidden")
-  }
-  const clickHandler2 = () => {
-    document.getElementById("right2").classList.toggle("hidden")
-    document.getElementById("down2").classList.toggle("hidden")
-    document.getElementById("ans2").classList.toggle("hidden")
-  }
-  const clickHandler3 = () => {
-    document.getElementById("right3").classList.toggle("hidden")
-    document.getElementById("down3").classList.toggle("hidden")
-    document.getElementById("ans3").classList.toggle("hidden")
-  }
-
-
-
   const FAQ = [
     {
       question: "How many bones does a cat have?",
@@ -41,8 +23,23 @@ function App() {
 
   const [faq, setFaq] = useState([0]);
   const [ans, setAns] = useState([0]);
+  const [toggle, setToggle] = useState(true)
+  const [toggle2, setToggle2] = useState(true)
+  const [toggle3, setToggle3] = useState(true)
 
 
+  const clickHandler = () => {
+    setToggle(!toggle)
+
+  }
+  const clickHandler2 = () => {
+    setToggle2(!toggle2)
+
+  }
+  const clickHandler3 = () => {
+    setToggle3(!toggle3)
+
+  }
   useEffect(() => {
 
     setFaq(FAQ.map(d => <div key={d.question}>{d.question}</div>))
@@ -59,29 +56,29 @@ function App() {
 
       <div onClick={clickHandler} className='faq'>
         <div className='flex'>
-          <span id='right' className='hidden icon'><FontAwesomeIcon icon={faAngleRight} /></span>
-          <span id='down' className='icon' ><FontAwesomeIcon icon={faAngleDown} /></span>
+          <span  className={`${(toggle) ? "icon" : "hidden"}`} ><FontAwesomeIcon icon={faAngleDown} /></span>
+          <span  className={`${(toggle) ? "hidden" : "icon"}`}><FontAwesomeIcon icon={faAngleRight} /></span>
           <span className='text'>{faq[0]}</span>
         </div>
-        <span id='ans' className='a gray text'>{ans[0]}</span>
+        <span className={`${(toggle) ? "gray text" : "hidden"}`} >{ans[0]}</span>
       </div>
 
       <div onClick={clickHandler2} className='faq'>
         <div className='flex'>
-          <span id='right2' className='icon'><FontAwesomeIcon icon={faAngleRight} /></span>
-          <span id='down2' className='hidden icon' ><FontAwesomeIcon icon={faAngleDown} /></span>
+          <span  className={`${(toggle2) ? "icon" : "hidden"}`} ><FontAwesomeIcon icon={faAngleRight} /></span>
+          <span  className={`${(toggle2) ? "hidden" : "icon"}`} ><FontAwesomeIcon icon={faAngleDown} /></span>
           <span className='text'>{faq[1]}</span>
         </div>
-        <span id='ans2' className='hidden gray text'>{ans[1]}</span>
+        <span id='ans2' className={`${(toggle2) ? "hidden" : "gray text"}`}>{ans[1]}</span>
       </div>
 
       <div onClick={clickHandler3} className='faq'>
         <div className='flex'>
-          <span id='right3' className='icon'><FontAwesomeIcon icon={faAngleRight} /></span>
-          <span id='down3' className='hidden icon' ><FontAwesomeIcon icon={faAngleDown} /></span>
+        <span  className={`${(toggle3) ? "icon" : "hidden"}`} ><FontAwesomeIcon icon={faAngleRight} /></span>
+          <span  className={`${(toggle3) ? "hidden" : "icon"}`} ><FontAwesomeIcon icon={faAngleDown} /></span>
           <span className='text'>{faq[2]}</span>
         </div>
-        <span id='ans3' className='hidden gray text'>{ans[2]}</span>
+        <span  className={`${(toggle3) ? "hidden" : "gray text"}`}>{ans[2]}</span>
       </div>
 
     </div>
